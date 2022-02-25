9dd6fd5211f1d9c8c4113202beccc5b9af2c615a
Kohei-kun-no <k.minoda1205@iCloud.com>
2022-02-25 18:41:08 +0900

    Storageの保護

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( //Material Designのアプリ
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
